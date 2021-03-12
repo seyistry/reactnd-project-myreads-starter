01495e21f785b3667c8cfa0448f999e015611718
seyi egunjobi <seyistry@gmail.com>
2021-03-12 12:54:57 +0100

    now indicate book location on main page.

--- src/component/bookShelfState.js
+++ src/component/bookShelfState.js
@@ -2,9 +2,12 @@ import React, { Component } from "react";
 
 class BookShelfState extends Component {
     render() {
-        const {books, updateBook } = this.props;
+        const { books, updateBook } = this.props;
         return (
-            <select onChange={(event) => updateBook(books, event.target.value)}>
+            <select
+                value={books.shelf}
+                onChange={(event) => updateBook(books, event.target.value)}
+            >
                 <option value="move" disabled>
                     Move to...
                 </option>
