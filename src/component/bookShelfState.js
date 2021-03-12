f4e2a3ae770c8dc6b86128a6cc2e3134d083b4a7
seyi egunjobi <seyistry@gmail.com>
2021-03-12 12:22:04 +0100

    create a child component for shelf selector

---
+++ src/component/bookShelfState.js
@@ -0,0 +1,22 @@
+import React, { Component } from "react";
+
+class BookShelfState extends Component {
+    render() {
+        const {books, updateBook } = this.props;
+        return (
+            <select onChange={(event) => updateBook(books, event.target.value)}>
+                <option value="move" disabled>
+                    Move to...
+                </option>
+                <option value="currentlyReading">Currently Reading</option>
+                <option value="wantToRead">Want to Read</option>
+                <option value="read">Read</option>
+                <option value="none" selected>
+                    None
+                </option>
+            </select>
+        );
+    }
+}
+
+export default BookShelfState;
